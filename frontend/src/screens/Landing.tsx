f122e02fa2b36360bee0f780542e09a9c4be36b7
mohakksingh <singhmohak999@gmail.com>
2024-12-03 02:27:47 +0530

    first commit

---
+++ frontend/src/screens/Landing.tsx
@@ -0,0 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import { Button } from "../components/Button";
+
+export const Landing = () => {
+
+    const navigate=useNavigate()
+  return (
+    <div className="flex justify-center">
+      <div className="pt-8 max-w-2xl">
+        <div className="grid grid-cols-1 gap-20 md:grid-cols-2">
+          <div className="flex justify-center">
+            <img src={"/chessboard.jpeg"} alt="" className="max-w-96" />
+          </div>
+          <div className="pt-16">
+            <div className="flex justify-center">
+              <h1 className="text-4xl font-bold text-white">
+                Play chess Online on the #2 Site!
+              </h1>
+            </div> 
+            <div className="mt-8 flex justify-center ">
+                <Button onClick={()=>navigate("/game")}>Play Online 
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
